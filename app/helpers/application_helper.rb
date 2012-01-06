8b32093b5edd876309617251f3a2e8d146a92fc5
stephen <root@lmsnhost>
2012-01-06 17:20:03 -0500

    add simple user navigation and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alertbox(name)
+    if name == :alert or name == :error
+      "alert-box error" #red
+    elsif name == :notice
+      "alert-box success" #green
+    elsif name == :yellow
+      "alert-box warning" #yellow
+    else
+      "alert-box" #grey
+    end
+  end
 end
