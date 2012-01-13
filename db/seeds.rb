05367362256340a17a5bf4a57b438addd4fd5852
stephen <root@lmsnhost>
2012-01-13 14:16:45 -0500

    Add admins and sample dj to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP TEST USER LOGIN'
-user = User.create! :email => 'test@letmesingnow.com', :password => 'testpw', :password_confirmation => 'testpw'
-puts 'New user created: ' << user.email
+puts 'SETTING UP INITIAL USERS'
+
+user = User.create :email => 'alex@letmesingnow.com', :password => 'alex', :password_confirmation => 'alex', :role => 'admin'
+puts 'New user created: ' << user.name
+
+user = User.create :email => 'stephen@letmesingnow.com', :password => 'lmsn', :password_confirmation => 'lmsn', :role => 'admin'
+puts 'New user created: ' << user.name
+
+user = User.create(:email => 'sampledj1@letmesingnow.com', :password => 'sampledj1', :password_confirmation => 'sampledj1', :role => 'dj')
+user.create_dj(:name => 'Sample Dj 1')
+user.dj.songs.create(:artist => 'artist1', :title => 'title1')
+puts 'New user created: ' << user.name
