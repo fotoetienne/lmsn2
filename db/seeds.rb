5ea85e22d9755a9ae22e336317ed8bfffb1593b8
stephen <root@lmsnhost>
2012-01-21 11:46:09 -0500

    Add some db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,16 @@ puts 'New user created: ' << user.name
 user = User.create(:email => 'sampledj1@letmesingnow.com', :password => 'sampledj1', :password_confirmation => 'sampledj1', :role => 'dj')
 user.create_dj(:name => 'Sample Dj 1')
 user.dj.songs.create(:artist => 'artist1', :title => 'title1')
+user.dj.songs.create(:artist => 'artist2', :title => 'title2')
 puts 'New user created: ' << user.name
+
+user = User.create(:email => 'sampledj2@letmesingnow.com', :password => 'sampledj2', :password_confirmation => 'sampledj2', :role => 'dj')
+user.create_dj(:name => 'Sample Dj 2')
+user.dj.songs.create(:artist => 'artist21', :title => 'title21')
+user.dj.songs.create(:artist => 'artist22', :title => 'title22')
+puts 'New user created: ' << user.name
+
+user = User.create(:email => 'singer1@letmesingnow.com', :password => 'singer1', :password_confirmation => 'singer1', :role => 'singer')
+user.create_singer(:name => 'Singer 1')
+puts 'New user created: ' << user.name
+
