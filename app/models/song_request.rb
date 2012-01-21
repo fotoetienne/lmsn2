6d5ec7c2586a7a8c815173bfb9d014e88e3bfb37
stephen <root@lmsnhost>
2012-01-21 12:21:08 -0500

    Fixed error 'stack level too deep'

--- app/models/song_request.rb
+++ app/models/song_request.rb
@@ -7,7 +7,7 @@ class SongRequest < ActiveRecord::Base
   validates :song_id  ,   :presence => true
   validates :singer_name, :presence => {:unless => "singer_id", :message => "The dj needs to know what name to call!"}
 
-  def singer_name
+  def name
     if singer_name
       singer_name
     elsif singer_id and singer.name
