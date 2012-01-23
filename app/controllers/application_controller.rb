163db63aa47f954435bb834fe5fd53ac89c0fea0
stephen <root@lmsnhost>
2012-01-23 14:03:44 -0500

    cancan redirects back if access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_path, :alert => exception.message
+    if request.referer
+      redirect_to :back, :alert => exception.message
+    else
+      redirect_to main_app.root_path, :alert => exception.message
+    end
   end
 end
