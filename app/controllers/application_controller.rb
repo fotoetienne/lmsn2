c471461ea847726680835c2e10dda5fc23e54698
stephen <root@lmsnhost>
2012-01-23 14:14:00 -0500

    cleaner cancan redirect syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    if request.referer
-      redirect_to :back, :alert => exception.message
-    else
-      redirect_to main_app.root_path, :alert => exception.message
-    end
+    redirect_to request.referer ? :back : main_app.root_path, :alert => exception.message
+#   if request.referer
+#     redirect_to :back, :alert => exception.message
+#   else
+#     redirect_to main_app.root_path, :alert => exception.message
+#   end
   end
 end
