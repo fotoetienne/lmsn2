b7adc60e3bb3911c00dc6017c94bfc83fe46ac59
stephen <root@lmsnhost>
2012-02-09 12:02:16 -0500

    debug jquery_mobile_helper

--- app/helpers/jquery_mobile_helper.rb
+++ app/helpers/jquery_mobile_helper.rb
@@ -1,47 +1,8 @@
-=begin
-require "action_view"
-
-module ActionView
-  module Helpers
-    module TagHelper
-      ATTRIBUTES = [:theme, :collapsed, :transition, :direction, :ajax, :role, :icon, :position, :inset]
-
-      def content_tag_with_mobile(name, content_or_options_with_block = nil, options = nil, escape = true, &block)
-        if block_given?
-          options = mobile_parse_options content_or_options_with_block
-          content_tag_without_mobile name, options, options, escape do
-            yield
-        end
-        else
-          options = mobile_parse_options options
-          content_tag_without_mobile name, content_or_options_with_block, options, escape
-        end
-      end
-
-
-      alias_method_chain :content_tag, :mobile
-
-      def mobile_parse_options(options=nil)
-        unless options.nil?
-          options.each do |key, value|
-            if ATTRIBUTES.include?(key.to_sym)
-              options.delete(key)
-              options["data-#{key}"] = value
-            end
-          end
-        end
-        return options
-      end
-    end
-  end
-end
-=end
-
-# Based on https://github.com/wakeless/rails-jquery-mobile
+# Based on rails-jquery-mobile gem <https://github.com/wakeless/rails-jquery-mobile>
 module JqueryMobileHelper
   ATTRIBUTES = [:theme, :collapsed, :transition, :direction, :ajax, :role, :icon, :position, :inset]
-  [:jqm_page, :jqm_navbar, :jqm_content, :jqm_footer, :jqm_header, :jqm_collapsible].each do |name|
-    class_eval "def #{name} (options = {}, &content)
+  [:page, :navbar, :content, :footer, :header, :collapsible].each do |name|
+    class_eval "def jqm_#{name} (options = {}, &content)
       mobile_div('#{name}', options) do
         yield content
       end
@@ -49,7 +10,7 @@ module JqueryMobileHelper
   end
 
   def jqm_listview(options = {})
-    mobile_tag :ul, {:role => "listview", :theme => "g"},options do
+    mobile_tag :ul, {:role => "listview"},options do
       yield if block_given?
     end
   end
