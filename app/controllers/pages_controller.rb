fced8d13d3469a02e97eaaf332c0f21b1b4ef6d6
stephen <root@lmsnhost>
2012-02-12 00:55:55 -0500

    add dj_home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     if user_signed_in?
       if current_user.dj?
-        render :dj
+        render 'dj_home'
       elsif current_user.singer?
         render 'singer_home'
       end
