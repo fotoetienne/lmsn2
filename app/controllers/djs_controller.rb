c6751afda1a5a17927891ba4c0879f7e6c463086
stephen <root@lmsnhost>
2012-02-17 13:25:20 -0500

    Simplify DJ views/controller

--- app/controllers/djs_controller.rb
+++ app/controllers/djs_controller.rb
@@ -22,36 +22,9 @@ class DjsController < ApplicationController
     end
   end
 
-  # GET /djs/new
-  # GET /djs/new.json
-  def new
-    @dj = Dj.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @dj }
-    end
-  end
-
   # GET /djs/1/edit
   def edit
     @dj = Dj.find(params[:id])
-  end
-
-  # POST /djs
-  # POST /djs.json
-  def create
-    @dj = Dj.new(params[:dj])
-
-    respond_to do |format|
-      if @dj.save
-        format.html { redirect_to @dj, notice: 'Dj was successfully created.' }
-        format.json { render json: @dj, status: :created, location: @dj }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @dj.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PUT /djs/1
@@ -61,7 +34,7 @@ class DjsController < ApplicationController
 
     respond_to do |format|
       if @dj.update_attributes(params[:dj])
-        format.html { redirect_to [:edit,@dj], notice: 'Your preferences were successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Your preferences were successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
